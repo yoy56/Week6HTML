df69f66df412e2de949efd65f2ab3ceb18d260e0
Rory B-J <roryboy1102@gmail.com>
2021-11-11 11:38:15 -0600

    Added a template for makeInitial function

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const URLofServer = "";
 
 class Item {
-    constructor(){
-
+    constructor(name){
+        this.name = name;
+        this.subitems = [];
     }
+};
+
+class SubItem {
+    constructor(name,info){
+        this.name = name
+        this.info = info
+    }
+};
+
+function makeInitial(){
+    //Make Inital Items
+    //console.log(items);
+    //Post Items
 };
 
 
