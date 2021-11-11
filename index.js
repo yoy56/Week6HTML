0d554c1a7691ebb59d3c450db6b05d2a3de7b353
Rory B-J <roryboy1102@gmail.com>
2021-11-11 16:34:51 -0600

    +Functionailty to Create Button-Removed Tempserver

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URLofServer = "";
+const URLofServer = "https://crudcrud.com/api/76033e82691b4830adcb47537793ec7f/people";
 
 let tempServer = [];
 
@@ -26,8 +26,8 @@ class Appointment {
 
 class DOMManager {
 
-    static renderDOM(){
-        let items = AjaxManager.pullItems();
+    static renderDOM(itemgot){
+        let items = itemgot;
         let itemList = $('#item-List');
         itemList.empty();
         for (let i = 0; i < items.length; i++) {
@@ -73,42 +73,48 @@ class DOMManager {
             }
         } 
     }
-}
 
-{/* <div id="item-List" class="container mt-5 mx-auto bg-secondary bg-gradient pb-3">
-        <!-- Exsample of Item in List (Remove later) -->
-        <div id="item-i"> <!-- "i" will be an id number -->
-            <h3>Kevin's Pets</h3> <!--Owner of Animal-->
-            <button class="btn btn-danger btn-sm" id="Del-btn-i"> Delete</button> <br>
-            <input type="text" name="subItemName" id="subItem-Name" placeholder="Subitem Name"> 
-            <input type="text" name="subItemInfo" id="subItem-Info" placeholder="Subitem Info"> <br>
-            <button id="addSub-btn-i" class="btn btn-primary my-2 w-100">Add Subitem</button>
-            <div id="subList-i" class="bg-light bg-gradient"> <!-- List of Subitems -->
-                <div id="subitem-i-0" class="my-3">  <!-- "0" will be the subitem's postion in the list -->
-                    <span class="fw-bold">Name: </span> <span>Teo</span> <!--Pet-->
-                    <span> - </span>
-                    <span class="fw-bold">Animal: </span> <span>Bird</span> <!--Type of Animal-->
-                    <button class="btn btn-danger btn-sm" id="sub-Del-btn-i-0">Delete SubItem</button>
-                    <div class="container" id="subSubList-i-0"> <!--Appointments-->
-                        <div class="" id="subSubItem-i-0-0">
-                            <span class="fw-bold">Appointment:</span>
-                            <span> 2:30pm 11/11/2021</span> <!--Date-->
-                            <div class="fw-bold">Notes:</div> <!--Info about the Appointment-->
-                            <div class="container bg-white border border-secondary">
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto deleniti 
-                                illo delectus corrupti voluptates pariatur amet harum consectetur ipsum ut 
-                                atque, rerum dolorem maiores non voluptatem eveniet autem quisquam sint.</p>
-                            </div>
-                            <button class="btn btn-warning btn-sm" id="subSub-Del-btn-i-0-0">Cancel Appointment</button>
-                        </div>
-                    </div>
-                </div> */}
+    static pullItems() {
+        AjaxManager.pullItems().then( (item) => this.renderDOM(item));
+    }
+
+    static addPerson(){
+        let name = $('#item-Name').val();
+        //Need to add test to make sure name is not blank
+        console.log(name);
+        AjaxManager.postItem(new Item(name),URLofServer).then( (item) => this.renderDOM(item));
+    }
+}
 
 class AjaxManager {
 
     static pullItems() {
-        //Pull from server
-        return tempServer;
+        //Pull from server 
+        console.log($.get(URLofServer));
+        return $.get(URLofServer);
+    }
+
+    static postItem(item,location) {
+        //Post to server
+        return $.ajax({
+            url: location,
+            type: 'POST',
+            dataType: 'json',
+            data: JSON.stringify(item),
+            contentType: "application/json"
+        });
+        
+    }
+
+    static putItem(item,location) {
+        
+        return $.ajax({
+            url: location,
+            type: 'PUT',
+            
+            data: JSON.stringify(item),
+            contentType: 'application/json'
+        });
     }
 }
 
@@ -123,10 +129,10 @@ function makeInitial(){
     let ownr1 = new Item("Harry");
     pt1.apponts.push(app1);
     ownr1.subitems.push(pt1);
-    tempServer.push(ownr1);
+    AjaxManager.postItem(ownr1,URLofServer).then( (resp) => console.log(resp));
 };
 
 
 $( () => {
-    //Get from sever Function
+    DOMManager.pullItems();
 });
